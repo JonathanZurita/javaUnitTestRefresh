f3723673ec97d38c671427c9687d08656bcf932d
JonathanZurita <jonrzurita@gmail.com>
2023-09-08 20:25:32 -0700

    add additional test

--- java-unit-test-refresher/src/main/java/unit/test/refresher/CommonMathFunctions.java
+++ java-unit-test-refresher/src/main/java/unit/test/refresher/CommonMathFunctions.java
@@ -1,5 +1,9 @@
 package unit.test.refresher;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 public class CommonMathFunctions {
 
     //Use the Math class four times to determine the logical expressions below.
@@ -24,5 +28,17 @@ public class CommonMathFunctions {
          */
         //your code here
         return 0.0;//REPLACE OR REMOVE THIS LINE
+    }
+
+    public ArrayList<Float> roundList(ArrayList<Float> floatList) {
+        /*
+        * TODO: round each value and return the arrayList in sorted ascending order;
+        *  length n = 9
+        *  num size 1 <= i <= 20
+        * BONUS: why can't you use an iterator or forEach to update the values of the arrayList?
+        * what is the tradeoff of using Collections vs creating a new arrayList?
+         */
+
+        return floatList;
     }
 }
--- java-unit-test-refresher/src/test/java/unit/test/refresher/CommonMathFunctionsTests.java
+++ java-unit-test-refresher/src/test/java/unit/test/refresher/CommonMathFunctionsTests.java
@@ -2,8 +2,13 @@ package unit.test.refresher;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.DisplayName;
 
 public class CommonMathFunctionsTests {
+    Logger logger = Logger.getLogger(String.valueOf(CommonMathFunctionsTests.class));
     CommonMathFunctions commonMathFunctions = new CommonMathFunctions();
     @Test
     public void doMath() {
@@ -18,5 +23,41 @@ public class CommonMathFunctionsTests {
         double value = commonMathFunctions.findPower(3.0,2.0);
 
         Assertions.assertEquals(9.0,value);
+    }
+
+    @DisplayName("ArrayList of Float is rounded and sorted")
+    @Test
+    public void roundList() {
+        ArrayList<Float> floatList = new ArrayList<>();
+        ArrayList<Float> expected = new ArrayList<>();
+        floatList.add((float) 2.6);
+        floatList.add((float) 2.2);
+        floatList.add((float) 1.6);
+        floatList.add((float) 9.4);
+        floatList.add((float) 7.45765);
+        floatList.add((float) 3.88889);
+        floatList.add((float) 1.999);
+        floatList.add((float) 6.56);
+        floatList.add((float) 4.44);
+
+        expected.add((float) 2);
+        expected.add((float) 2);
+        expected.add((float) 2);
+        expected.add((float) 3);
+        expected.add((float) 4);
+        expected.add((float) 4);
+        expected.add((float) 7);
+        expected.add((float) 7);
+        expected.add((float) 9);
+
+
+        ArrayList<Float> result = commonMathFunctions.roundList(floatList);
+
+        if(expected.equals(result)) {
+            logger.info("\n***********************************    FOLLOW UP QUESTION   ************************************\n\n " +
+                    "1) Why can you implicitly cast an int value to a primitive float, but not the other way around?\n\n" +
+                    "2) Why must you cast int to float or use the wrapper class Float() in order for this test to pass?");
+        }
+        Assertions.assertEquals(expected,result);
     }
 }
