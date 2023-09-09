3beb6655cf484901ba2c2faec210c0dc6bbfe596
JonathanZurita <jonrzurita@gmail.com>
2023-09-08 17:02:09 -0700

    adds junit and mockito dependencies

--- java-unit-test-refresher/src/main/java/unit/test/refresher/Main.java
+++ java-unit-test-refresher/src/main/java/unit/test/refresher/Main.java
@@ -2,6 +2,6 @@ package unit.test.refresher;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Java Unit Test Refresher");
     }
 }
